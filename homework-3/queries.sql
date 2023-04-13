b443d97ac3f31cf462184b6ace9daa68d8bfeab0
Михаил <misga.yats@gmail.com>
2023-04-13 18:06:09 +0200

    homework-3 is done

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,18 +2,27 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 select customers.company_name, CONCAT(first_name, ' ', last_name) as full_name
-from employees, customers, orders
-where employees.city = 'London' and customers.city = 'London' and orders.ship_via = 2
+from employees, customers
+join orders on ship_via = 2
+where employees.city = 'London' and customers.city = 'London'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-select product_name, units_in_stock, contact_name, phone
-from products, suppliers
-where products.discontinued = 0 and units_in_stock < 25
-and category_id = 2 or category_id = 4
-order by units_in_stock
+select
+	p.product_name,
+	p.units_in_stock,
+	p.supplier_id,
+	s.contact_name,
+	s.phone,
+	p.category_id,
+	c.category_name
+from products p
+join suppliers s on p.supplier_id = s.supplier_id
+join categories c on p.category_id = c.category_id and c.category_name in ('Dairy Products', 'Condiments')
+where p.discontinued = 0 and p.units_in_stock < 25
+order by p.units_in_stock;
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 select company_name
